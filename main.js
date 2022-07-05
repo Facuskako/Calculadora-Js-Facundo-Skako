ff803d87e46217a63d4f5ebc2b323d8f2d251f6f
Facundo Skako <facuskako@hotmail.com>
2022-07-05 16:59:16 -0300

    Entrega ciclos js

--- main.js
+++ main.js
@@ -3,7 +3,13 @@ let montoAPagar
 
     const numeroDeCoutas = [3, 6, 9, 12];
 
+
     for(let i = 0; i < 4; i++){
+        if(montoIngresado <= 0){
+            
+            alert ("Ingrese un monto valido, por favor!")
+            break
+        }
         montoAPagar = montoIngresado / numeroDeCoutas[i]
         alert("Con " + numeroDeCoutas[i] + " cuotas, Pagas " + montoAPagar + " $"); 
     }
